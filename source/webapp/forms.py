c43f0210be4f235cbbb72493e8f8003ae81bea7b
Archer <arseniy.tarapon@gmail.com>
2018-12-24 15:09:11 +0600

    Добавление CRUD для сущности food.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,3 +1,16 @@
 from django import forms
 from webapp.models import Food, Order, OrderFood
 
+
+class FoodCreateForm(forms.ModelForm):
+    class Meta:
+        model = Food
+        exclude = []
+
+
+class FoodUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Food
+        exclude = []
+
+
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,13 @@
 from webapp.models import Food, Order, OrderFood
 from django.views.generic import DetailView, CreateView, UpdateView, View, DeleteView, ListView
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
+from webapp.forms import FoodCreateForm, FoodUpdateForm
+
+
+class FoodListView(ListView):
+    model = Food
+    template_name = 'index.html'
 
 
 class FoodDetailView(DetailView):
@@ -9,10 +15,23 @@ class FoodDetailView(DetailView):
     template_name = 'food_detail.html'
 
 
+class FoodDeleteView(DeleteView):
+    model = Food
+    template_name = 'food_delete.html'
+
+
+class FoodUpdateView(UpdateView):
+    model = Food
+    template_name = 'food_update.html'
+    form_class = FoodUpdateForm
+
+
 class FoodCreateView(CreateView):
     model = Food
     template_name = 'food_create.html'
-    form_class = FoodForm
+    form_class = FoodCreateForm
 
     def get_success_url(self):
         return reverse('food_detail', kwargs={'pk': self.object.pk})
+
+
